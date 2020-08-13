d2804146f1560936b5583cd3ad5b15e8bddc4636
PhilippeMts <phi.metais@gmail.com>
2020-08-13 11:44:46 +0200

    :sparkles: Introducing about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,58 +2,38 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import informationImage from "../../static/images/Information-document.svg";
-import eventImage from "../../static/images/Event-managment.svg";
-import ProductivityImage from "../../static/images/Increase-Productivity.svg";
-import happinessImage from "../../static/images/Make-happiness.svg";
+import visionImage from "../../static/images/about-vision.svg"
+import missionImage from "../../static/images/about-mission.svg"
 
 const AboutPage = () => (
     <Layout>
         <SEO title="About"/>
 
         <div className={"page-header"}>
-            <h2>What’s HiStaff?</h2>
-            <p>HiStaff is a service that boost the staff’s productivity by offering new ways<br />to increase employee motivation and happiness.</p>
+            <h2>What is Evidence?</h2>
+            <p>Evidence is building an open network and set of tools<br/>to democratize the decentralization and certification of data.</p>
         </div>
 
         <div className={"container"}>
             <div className={"targets"}>
                 <div className={"title"}>
-                    <h2>Our Targets</h2>
+                    <h2>Our Statements</h2>
                 </div>
 
                 <div className={"row"}>
                     <div className={"col-6"}>
                         <div className={"item"}>
-                            <img alt={"Information"} src={informationImage}/>
-                            <h3>Handling of Employes’s Information and documents</h3>
-                            <p>Having a human management software for businesses and startups is not always cost-effective. HiStaff allows companies to archive their employees' documents and information free of charge.</p>
+                            <img alt={"Vision"} src={visionImage}/>
+                            <h3>Vision</h3>
+                            <p>At Evidence, we envision a world where data of individuals and entities are not trapped in closed ecosystems but managed by their rightful owners in open networks. Rethinking how data is certified and disclosed enables new forms of collaboration in a world of widespread trust. From this paradigm shift will emerge diverse, prosperous and independent services and applications.</p>
                         </div>
                     </div>
 
                     <div className={"col-6"}>
                         <div className={"item"}>
-                            <img alt={"Event"} src={eventImage}/>
-                            <h3>Manage Events</h3>
-                            <p>How to hold internal events is one of the problems of companies. HiStaff by presenting new ideas allows event management to be done as easily as possible.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={"row"}>
-                    <div className={"col-6"}>
-                        <div className={"item"}>
-                            <img alt={"Productivity"} src={ProductivityImage}/>
-                            <h3>Raise productivity</h3>
-                            <p>Many factors are affecting the staff's productivity in the workplace, the relationship between manager and staff and environmental factors are the main factors, therefore we will focus on these two factors in achieving our goal. Ultimately, increasing employee's productivity results in increasing the quality of products and services that are going to be provided.</p>
-                        </div>
-                    </div>
-
-                    <div className={"col-6"}>
-                        <div className={"item"}>
-                            <img alt={"Happiness"} src={happinessImage}/>
-                            <h3>Boost motivation and happiness</h3>
-                            <p>It is Obvious that a happy employee will make the environment happy and energetic! We are constantly attempting to provide solutions for maximizing employee’s motivation and joy.</p>
+                            <img alt={"Mission"} src={missionImage}/>
+                            <h3>Mission</h3>
+                            <p>Evidence is an infrastructure and service provider building a global network to securely own and exchange data at scale between individuals and entities. Evidence’s work relies on cutting-edge technologies to unleash the power of data with minimal disclosure.</p>
                         </div>
                     </div>
                 </div>
