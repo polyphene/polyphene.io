07a6ff2df761463334581f67845cbac473863d27
PhilippeMts <phi.metais@gmail.com>
2020-10-08 19:35:14 +0200

    🚨 Fix compiler / linter warnings.

--- src/components/blogCards.js
+++ src/components/blogCards.js
@@ -7,7 +7,7 @@ const BlogCards = ({posts}) => (
       posts.map(({ node }) => (
         <div className="col-4">
           <div className="card">
-            <img src={node.feature_image} className="card-img-top" alt="blog article featured image"/>
+            <img src={node.feature_image} className="card-img-top" alt="blog article featured"/>
             <div className="card-body">
               <h3 className="card-title">{node.title}</h3>
               <p className="card-text">{node.excerpt}</p>
